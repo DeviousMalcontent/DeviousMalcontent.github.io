94a5d1c3b461ddf427cf280dc845673064cb5a0c
Mark <deviousmalcontent@gmail.com>
2022-03-01 15:02:26 +1100

    Added some pages

--- _tools/MakeHeaders.bat
+++ _tools/MakeHeaders.bat
@@ -25,4 +25,9 @@ echo Graphics
 ImageMagick-7.0.11-12-portable-Q16-x86\magick -size 766x72 -define gradient:angle=0 gradient:"#788BFE-#1639FE" ..\images\graphics.png 
 ImageMagick-7.0.11-12-portable-Q16-x86\convert ..\images\graphics.png ( +clone  -alpha extract -draw "fill black polygon 0,0 0,10 10,0 fill white circle 10,10 10,0" ( +clone -flip ) -compose Multiply -composite ( +clone -flop ) -compose Multiply -composite ) -alpha off -compose CopyOpacity -composite  ..\images\graphics.png
 ImageMagick-7.0.11-12-portable-Q16-x86\convert -font arial -fill black -pointsize 60 -gravity West -draw "text 24,-4 '* Graphics'" ..\images\graphics.png ..\images\graphics.png
+
+echo HTML
+ImageMagick-7.0.11-12-portable-Q16-x86\magick -size 766x72 -define gradient:angle=0 gradient:"#788BFE-#1639FE" ..\images\html.png 
+ImageMagick-7.0.11-12-portable-Q16-x86\convert ..\images\html.png ( +clone  -alpha extract -draw "fill black polygon 0,0 0,10 10,0 fill white circle 10,10 10,0" ( +clone -flip ) -compose Multiply -composite ( +clone -flop ) -compose Multiply -composite ) -alpha off -compose CopyOpacity -composite  ..\images\html.png
+ImageMagick-7.0.11-12-portable-Q16-x86\convert -font arial -fill black -pointsize 50 -gravity West -draw "text 24,-4 '* HyperText Markup Language'" ..\images\html.png ..\images\html.png
 pause
